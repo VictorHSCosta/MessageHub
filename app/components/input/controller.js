9d77789790354a03ee2205978ccc27b355770ab5
VictorHSCosta <victorhenriquecosta23@gmail.com>
2026-06-14 14:44:49 -0300

    feat: refactor Input component for improved password handling and validation

--- app/components/input/controller.js
+++ app/components/input/controller.js
@@ -11,14 +11,19 @@ export default class extends Controller {
         const value = input.value
         const errors = []
 
-        if (value.length < 8) errors.push("at least 8 characters")
-        if (!/[A-Z]/.test(value)) errors.push("one uppercase letter")
-        if (!/[a-z]/.test(value)) errors.push("one lowercase letter")
-        if (!/\d/.test(value)) errors.push("one number")
+        if (value.length < 8) errors.push("A senha precisa conter pelo menos 8 caracteres")
+        if (!/[A-Z]/.test(value)) errors.push("A senha precisa conter pelo menos uma letra maiúscula")
+        if (!/[a-z]/.test(value)) errors.push("A senha precisa conter pelo menos uma letra minúscula")
+        if (!/\d/.test(value)) errors.push("A senha precisa conter pelo menos um número")
 
         if (errors.length > 0) {
             this.errorTarget.classList.remove("hidden")
-            this.errorTarget.textContent = `Password must include ${errors.join(", ")}.`
+            this.errorTarget.textContent = ``
+            errors.forEach(line => {
+                const li = document.createElement("li")
+                li.textContent = line
+                this.errorTarget.appendChild(li)
+            })
         } else {
             this.errorTarget.classList.add("hidden")
             this.errorTarget.textContent = ""
